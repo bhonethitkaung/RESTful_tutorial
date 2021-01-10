8363cf19ac268491ae37b8ab2318fc963e84e300
Bhone Thit Kaung <bhonethitkaung1999@gmail.com>
2021-01-10 17:37:32 +0630

    Initial Commit

---
+++ AlternateMavenProjectSetup/src/main/java/org/studyeasy/Name.java
@@ -0,0 +1,17 @@
+package org.studyeasy;
+
+public class Name {
+	private String firstName;
+	private String lastName;
+	public Name(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	@Override
+	public String toString() {
+		return "Name [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+}
